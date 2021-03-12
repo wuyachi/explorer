d54e97a0a2b121755f48e5438a8b096c9575b88f
gaoyuan tan <chain_dev@163.com>
2021-03-12 13:19:46 +0800

    add debug

--- controllers/mysql.go
+++ controllers/mysql.go
@@ -4,6 +4,7 @@ import (
 	"github.com/astaxie/beego"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -15,7 +16,12 @@ func newDB() *gorm.DB {
 	password := beego.AppConfig.String("mysqlpass")
 	url := beego.AppConfig.String("mysqlurls")
 	scheme := beego.AppConfig.String("mysqldb")
-	db, err := gorm.Open(mysql.Open(user+":"+password+"@tcp("+url+")/"+scheme+"?charset=utf8"), &gorm.Config{})
+	sqlDebug := beego.AppConfig.String("mysqldebug")
+	Logger := logger.Default
+	if sqlDebug == "true" {
+		Logger = Logger.LogMode(logger.Info)
+	}
+	db, err := gorm.Open(mysql.Open(user+":"+password+"@tcp("+url+")/"+scheme+"?charset=utf8"), &gorm.Config{Logger: Logger})
 	if err != nil {
 		panic(err)
 	}
